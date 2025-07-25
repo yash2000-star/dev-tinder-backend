611ac2d1447ec5163ef30de58a7d4941aa64c61c
Yash nirwan <yashnirwan18@gmail.com>
2025-07-25 18:32:54 +0530

    destructure chat

--- src/models/chat.js
+++ src/models/chat.js
@@ -27,5 +27,5 @@ const chatSchema = new mongoose.Schema(
 
 const Chat = mongoose.model("Chat", chatSchema);
 
-module.exports =  Chat 
+module.exports =  { Chat } 
 
--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
-const  Chat = require("../models/Chat");
+const  {Chat} = require("../models/Chat");
 
 const chatRouter = express.Router();
 
